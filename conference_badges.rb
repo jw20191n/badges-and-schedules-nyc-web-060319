8192ef36464b9c70a155f783787a31a280809be0
Jiayi Wang <clairejw1015@gmail.com>
2019-05-22 22:33:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  array.each do |name|
+def batch_badge_creator(attendees)
+  attendees.each do |name|
     "Hello, my name is #{name}."
   end
 end
